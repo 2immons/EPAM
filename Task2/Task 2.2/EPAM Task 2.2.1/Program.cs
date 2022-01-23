cad81ff2b2948217d95d8effb5dc2e39505fa0a6
Pavel <podoprigoraps@gmail.com>
2022-01-23 16:55:39 +0400

    2.2.2 basic functionality

--- Task2/Task 2.2/EPAM Task 2.2.1/Program.cs
+++ Task2/Task 2.2/EPAM Task 2.2.1/Program.cs
@@ -1,147 +1,169 @@
-﻿using System;
-using static System.Console;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Game
+﻿namespace Game
 {
     class Game
     {
-        private const int mapWidth = 40;
-        private const int mapHeigth = 20;
-        private const ConsoleColor BorderColor = ConsoleColor.Gray;
-        
+        private const int mapWidth = 30;
+        private const int mapHeigth = 30;
+
+        static void DrawField(char[,] field, Player player)
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int k = 0; k < mapHeigth; k++)
+                {
+                    Console.Write(field[i, k]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Adrenaline points = {player.AdrenalinePoints}");
+        }
+
+        static bool CheckForLoseOrVictory(List<Character> characters, List<Object> objects, Player player)
+        {
+            if (player.AdrenalinePoints < 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Adrenaline is < 0\nGame over!");
+                return false;
+            }
+
+            if (player.X == 0 || player.X == mapWidth - 1 || player.Y == 0 || player.Y == mapHeigth - 1)
+            {
+                Console.Clear();
+                Console.WriteLine("You left the field\nGame over!");
+                return false;
+            }
+
+            foreach (var item in characters)
+            {
+                if (item.X == player.X && item.Y == player.Y)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You was eaten by monster\nGame over!");
+                    return false;
+                }
+            }
+
+            foreach (var item in objects)
+            {
+                if (item.X == player.X && item.Y == player.Y)
+                {
+                    Type itemType = item.GetType();
+                    if (itemType.Equals(typeof(Obstacle)))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You hit an obstacle!\nGame over!");
+                        return false;
+                    }
+                }
+            }
+
+            int count = 0;
+            foreach (var item in objects)
+            {
+                if (item.GetType() == typeof(Bonus))
+                    count++;
+            }
+            if (count > 0)
+                return true;
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("You collect all bonuses!\nYou won!");
+                return false;
+            }
+        }
+
+        static char[,] MapInit(char[,] field, List<Character> characters, List<Object> objects, Player player)
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int k = 0; k < mapHeigth; k++)
+                {
+                    if (i == 0 || i == mapWidth - 1 || k == 0 || k == mapHeigth - 1)
+                        field[i, k] = '|';
+                    else field[i, k] = ' ';
+                }
+            }
+
+            field[player.X, player.Y] = player.Symbol;
+            player.AdrenalinePoints -= 2;
+
+            for (int i = objects.Count - 1; i >= 0; i--)
+            {
+                if (objects[i].X == player.X && objects[i].Y == player.Y)
+                {
+                    Type itemType = objects[i].GetType();
+                    if (itemType.Equals(typeof(AdrenalinePoint)))
+                    {
+                        player.AdrenalinePoints += 10;
+                        objects.Remove(objects[i]);
+                    }
+                    else if (itemType.Equals(typeof(Bonus)))
+                    {
+                        objects.Remove(objects[i]);
+                    }
+                }
+                else
+                {
+                    field[objects[i].X, objects[i].Y] = objects[i].Symbol;
+
+                }
+            }
+
+            objects.RemoveAll(item => item.X == 35);
+
+            foreach (var item in characters)
+                field[item.X, item.Y] = item.Symbol;
+
+            return field;
+        }
+
+        static void CharacterMovements(List<Character> characters, Player player)
+        {
+            player.Move();
+            foreach (var item in characters)
+                item.Move();
+        }
+
         static void Main()
         {
-            SetWindowSize(mapWidth,
-                          mapHeigth);
-            SetBufferSize(mapWidth,
-                          mapHeigth);
-            CursorVisible = false;
+            Player player = new();
 
-            DrawBorder();
+            Random rnd = new();
 
-            Character Person = new Character(3,4);
-
-            DrawCharacter();
-
-            ReadKey();
-        }
-
-        static void DrawCharacter()
-        {
-            int x = Character.Move()[0];
-            int y = Character.Move()[1];    
-
-
-
-            for (int i = 0; i < mapWidth; i++)
+            List<Character> characters = new()
             {
-                new Pixel(i, 0, BorderColor).Draw();
-                new Pixel(i, mapHeigth - 1, BorderColor).Draw();
-            }
-            for (int i = 0; i < mapHeigth; i++)
+                new Zombie(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new Wolf(rnd.Next(1, 28), rnd.Next(1, 28))
+            };
+
+            List<Object> objects = new()
             {
-                new Pixel(0, i, BorderColor).Draw();
-                new Pixel(mapWidth - 1, i, BorderColor).Draw();
-            }
-        }
-        static void DrawBorder()
-        {
-            for (int i = 0; i < mapWidth; i++)
+                new Bonus(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new Bonus(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new Bonus(rnd.Next(1, 28), rnd.Next(1, 28)),
+
+                new AdrenalinePoint(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new AdrenalinePoint(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new AdrenalinePoint(rnd.Next(1, 28), rnd.Next(1, 28)),
+
+                new Obstacle(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new Obstacle(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new Obstacle(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new Obstacle(rnd.Next(1, 28), rnd.Next(1, 28)),
+                new Obstacle(rnd.Next(1, 28), rnd.Next(1, 28))
+            };
+
+            char[,] field = new char[mapWidth, mapHeigth];
+
+            DrawField(MapInit(field, characters, objects, player), player);
+            while (CheckForLoseOrVictory(characters, objects, player))
             {
-                new Pixel(i, 0, BorderColor).Draw();
-                new Pixel(i, mapHeigth - 1, BorderColor).Draw();
+                CharacterMovements(characters, player);
+                Console.Clear();
+                DrawField(MapInit(field, characters, objects, player), player);
             }
-            for (int i = 0; i < mapHeigth; i++)
-            {
-                new Pixel(0, i, BorderColor).Draw();
-                new Pixel(mapWidth - 1,i, BorderColor).Draw();
-            }
-        }
-
-        // игрок - P
-        // зомби - Z
-        // препятствие - #
-        // бонус - $
-    }
-
-    public struct Pixel
-    {
-        public Pixel(int x, int y, ConsoleColor color)
-        {
-            X = x;
-            Y = y;
-            Color = color;
-
-        }
-        public int X { get; set; }
-        public int Y { get; set; }
-        public ConsoleColor Color { get; }
-        public void Draw()
-        {
-            Console.SetCursorPosition(X, Y);
-            Console.Write('@');
-
-        }
-        public void Clear()
-        {
-            Console.SetCursorPosition(X, Y);
-            Console.Write(' ');
-            }
-        }
-
-    class Character
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public char symbol = 'P';
-
-        public Character(int X, int Y)
-        {
-            this.X = X;
-            this.Y = Y;
-        }
-        public static int[] Move()
-        {
-            int[] coordinates = new int[2];
-            ConsoleKey Key = Console.ReadKey().Key;
-            switch (Key)
-            {
-                case ConsoleKey.A:
-                    coordinates[0] = -1;
-                    coordinates[1] = 0;
-                    break;
-                case ConsoleKey.D:
-                    coordinates[0] = 1;
-                    coordinates[1] = 0;
-                    break;
-                case ConsoleKey.W:
-                    coordinates[0] = 0;
-                    coordinates[1] = 1;
-                    break;
-                case ConsoleKey.S:
-                    coordinates[0] = 0;
-                    coordinates[1] = -1;
-                    break;
-                default:
-                    break;
-            }
-            return coordinates;
         }
     }
 }
-
-//class Zombie : Character
-//{
-//    public int X { get; set; }
-//    public int Y { get; set; }
-//    public char Symbol { get; set; }
-//}
-
-//class Player : Character
-//{
-//    public int X { get; set; }
-//    public int Y { get; set; }
-//    public char Symbol { get; set; }
-//}
