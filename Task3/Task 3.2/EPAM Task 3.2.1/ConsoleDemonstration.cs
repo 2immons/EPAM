9527ef48feed582627d9ac8bceb100f07c6a4bf9
Pavel <podoprigoraps@gmail.com>
2022-02-13 20:42:23 +0400

    task 3.2.1 done

---
+++ Task3/Task 3.2/EPAM Task 3.2.1/ConsoleDemonstration.cs
@@ -0,0 +1,23 @@
+﻿namespace EPAM_3_2_1
+{
+    class ConsoleDemonstration
+    {
+        static void Main()
+        {
+            // some tests
+            var arr = new DynamicArray<int>();
+            Console.WriteLine(arr.Capacity);
+            Console.WriteLine(arr.Length);
+            arr.Add(12);
+            arr.Add(12);
+            Console.WriteLine(arr[1]);
+            arr.Insert(1, 10);
+            Console.WriteLine(arr[1]);
+            arr.Remove(1);
+            Console.WriteLine(arr[1]);
+            Console.WriteLine(arr.Capacity);
+            Console.WriteLine(arr.Length);
+        }
+
+    }
+}
